c8cb97005da105c29ac8fc7c1f44a4985dea54ac
ap-novaro <134993270+ap-novaro@users.noreply.github.com>
2023-06-02 15:33:15 +0530

    Update main.tf

--- dev/main.tf
+++ dev/main.tf
@@ -1,8 +1,17 @@
+### Terraform state file storage bucket code Don't Edit ###
+terraform {
+  backend "gcs" {
+    bucket      = "novaro-terraform-state-bucket-demo"
+    prefix      = "terraform/state"
+  }
+}
+
 provider "google" {
   project     = "novaro-dev-2023"
   region      = "us-central-1"
 }
 
+## Storage_Bucket_Code ##
 resource "google_storage_bucket" "my_bucket" {
   name          = "novaro-test-bucket-02"
   location      = "us-central1"
