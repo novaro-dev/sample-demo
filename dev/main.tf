cc315b812e2f72b85ea14a7e9c86045cbd961e5a
ap-novaro <134993270+ap-novaro@users.noreply.github.com>
2023-06-02 15:35:38 +0530

    Update main.tf

--- dev/main.tf
+++ dev/main.tf
@@ -13,6 +13,6 @@ provider "google" {
 
 ## Storage_Bucket_Code ##
 resource "google_storage_bucket" "my_bucket" {
-  name          = "novaro-test-bucket-00"
+  name          = "novaro-test-bucket-11"
   location      = "us-central1"
 }
